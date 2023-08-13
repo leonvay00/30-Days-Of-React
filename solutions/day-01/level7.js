8fd859e91a57006c96ab62cbaa9de36de1be9fe9
leon-vay <leonvay00@gmail.com>
2023-08-13 12:54:24 -0700

    finished level 7

--- solutions/day-01/level7.js
+++ solutions/day-01/level7.js
@@ -54,6 +54,24 @@ const users = {
     }
   }
 
+const countries = {
+  "United States": {
+    "population": 332900000,
+    "capital": "Washington, D.C.",
+    "language": "English"
+  },
+  "China": {
+    "population": 1444216107,
+    "capital": "Beijing",
+    "language": "Mandarin Chinese"
+  },
+  "India": {
+    "population": 1380004385,
+    "capital": "New Delhi",
+    "language": "Hindi"
+  }
+}
+
 // 1. Find the person who has many skills in the users object
 let skillCount = 0
 let user = null
@@ -75,7 +93,7 @@ for (const key in users) {
         loggedIn++
     }
 
-    if (user[key].points >= 50) {
+    if (users[key].points >= 50) {
         usersWithOverFifty++
     }
 }
@@ -83,4 +101,40 @@ for (const key in users) {
 console.log(loggedIn)
 console.log(usersWithOverFifty)
 
-// 3. 
+// 3. Find people who are MERN stack developer from the users object
+const mernDevs = []
+
+for (const key in users) {
+  const curUserSkills = users[key].skills
+  if (curUserSkills.includes("MongoDB") && curUserSkills.includes("Express") && curUserSkills.includes('React') && curUserSkills.includes('Node')) {
+    mernDevs.push(key)
+  }
+}
+
+console.log(mernDevs)
+
+// 4. Set your name in the users object without modifying the original users object
+users.Leon = {
+  email: 'leon@leon.com',
+  skills: ['Python', 'Express', 'Node', 'Java', 'HTML', 'CSS', 'JavaScript', 'React', 'Angular'],
+  age: 23,
+  isLoggedIn: false,
+  points: 50
+}
+
+// 5. Get all keys or properties of users objects
+const userNames = Object.keys(users)
+console.log(userNames)
+
+// 6. Get all the values of users object
+const userValues = Object.values(users)
+console.log(userValues)
+
+// 7. Use the countries object to print a country name, capital, population, and language
+for (const key in countries) {
+  let name = key
+  let capital = countries[key].capital
+  let population = countries[key].population
+  let language = countries[key].language
+  console.log(`${name}'s capital is ${capital}. The population count is ${population} and they speak ${language}`)
+}
