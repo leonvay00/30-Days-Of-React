fa426c54273e291e07234b743ac187b33b40a077
leon-vay <leonvay00@gmail.com>
2023-08-17 12:12:08 -0700

    Formatted code Better

--- solutions/day-01/level1.js
+++ solutions/day-01/level1.js
@@ -6,7 +6,7 @@
 const empty = [];
 
 // 2. Declare an array with more than 5 number of elements
-const names = ['John', 'Jack', 'Betty', 'Tom', 'Pam', 'Sam'];
+const names = ["John", "Jack", "Betty", "Tom", "Pam", "Sam"];
 
 // 3. Find the length of your array
 let arraySize = names.length;
@@ -20,7 +20,15 @@ let last = names[names.length - 1];
 const mixedDataTypes = [1, "hello", false, [], null, undefined];
 
 // 6. Declare an array variable name itCompanies and assign intial values Facebook, Google, Microsoft, Apple, IBM, Oracle and Amazon
-const itCompanies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'];
+const itCompanies = [
+  "Facebook",
+  "Google",
+  "Microsoft",
+  "Apple",
+  "IBM",
+  "Oracle",
+  "Amazon",
+];
 
 // 7. Print the array using console.log()
 console.log(itCompanies);
@@ -43,35 +51,37 @@ console.log(itCompanies[5]);
 console.log(itCompanies[6]);
 
 // 11. Change each company name to uppercase one by one and print them out
-itCompanies[0] = 'FACEBOOK';
+itCompanies[0] = "FACEBOOK";
 console.log(itCompanies[0]);
 
-itCompanies[1] = 'GOOGLE';
+itCompanies[1] = "GOOGLE";
 console.log(itCompanies[1]);
 
-itCompanies[2] = 'MICROSOFT';
+itCompanies[2] = "MICROSOFT";
 console.log(itCompanies[2]);
 
-itCompanies[3] = 'APPLE';
+itCompanies[3] = "APPLE";
 console.log(itCompanies[3]);
 
-itCompanies[4] = 'IBM';
+itCompanies[4] = "IBM";
 console.log(itCompanies[4]);
 
-itCompanies[5] = 'ORACLE';
+itCompanies[5] = "ORACLE";
 console.log(itCompanies[5]);
 
-itCompanies[6] = 'AMAZON';
+itCompanies[6] = "AMAZON";
 console.log(itCompanies[6]);
 
 // 12. Print the array like as a sentence: Facebook, Google, Microsoft, Apple, IBM, Oracle, and Amazon are big IT companies
-console.log(`${itCompanies[0]}, ${itCompanies[1]}, ${itCompanies[2]}, ${itCompanies[3]}, ${itCompanies[4]}, ${itCompanies[5]}, and ${itCompanies[6]} are big IT companies `);
+console.log(
+  `${itCompanies[0]}, ${itCompanies[1]}, ${itCompanies[2]}, ${itCompanies[3]}, ${itCompanies[4]}, ${itCompanies[5]}, and ${itCompanies[6]} are big IT companies `
+);
 
 // 13. Check if a certain company exists in the itCompanies array. If it exist return the company else return a company is not found
 if (itCompanies.includes("Github")) {
-    console.log(itCompanies[itCompanies.indexOf("Github")]);
+  console.log(itCompanies[itCompanies.indexOf("Github")]);
 } else {
-    console.log("Company is not found");
+  console.log("Company is not found");
 }
 
 // 14. Filter out companies which have more than one 'o' wihout the filter method
@@ -91,7 +101,7 @@ itCompanies.slice(3);
 itCompanies.slice(-3);
 
 // 19. Slice out the middle IT company or companies from the array
-itCompanies.slice(itCompanies.length / 2, (itCompanies.length / 2) + 1);
+itCompanies.slice(itCompanies.length / 2, itCompanies.length / 2 + 1);
 
 // 20. Remove the first IT company from the array
 itCompanies.shift();
--- solutions/day-01/level5.js
+++ solutions/day-01/level5.js
@@ -3,17 +3,17 @@
  */
 
 // 1. Write code which can give grades to students according to theirs scores
-let score = 80
+let score = 80;
 if (80 <= score <= 100) {
-    console.log("A")
+  console.log("A");
 } else if (70 <= score <= 79) {
-    console.log("B")
+  console.log("B");
 } else if (60 <= score <= 69) {
-    console.log("C")
+  console.log("C");
 } else if (50 <= score <= 59) {
-    console.log("D")
+  console.log("D");
 } else {
-    console.log("F")
+  console.log("F");
 }
 
 /**
@@ -24,49 +24,59 @@ if (80 <= score <= 100) {
  * - June, July, or August, the season is Summer
  */
 
-let month = prompt("Enter the current month: ")
-const autumn = ["september", "october", "november"]
-const winter = ["december", "january", "february"]
-const spring = ["march", "april", "may"]
-const summer = ["june", "july", "august"]
-month = month.toLowerCase()
-
+let month = prompt("Enter the current month: ");
+const autumn = ["september", "october", "november"];
+const winter = ["december", "january", "february"];
+const spring = ["march", "april", "may"];
+const summer = ["june", "july", "august"];
+month = month.toLowerCase();
 
 if (autumn.includes(month)) {
-    console.log("The season is Autumn")
+  console.log("The season is Autumn");
 } else if (winter.includes(month)) {
-    console.log("The season is Winter")
+  console.log("The season is Winter");
 } else if (spring.includes(month)) {
-    console.log("The season is Spring")
+  console.log("The season is Spring");
 } else if (summer.includes(month)) {
-    console.log("The season is Summer")
+  console.log("The season is Summer");
 } else {
-    console.log("That month does not exist")
+  console.log("That month does not exist");
 }
 
 // 3. Check is a day is a weekend day or a working day. Your script will take day as an input
-let day = prompt("What is the day today: ").toLowerCase()
-day = day.charAt(0).toUpperCase() + day.slice(1)
+let day = prompt("What is the day today: ").toLowerCase();
+day = day.charAt(0).toUpperCase() + day.slice(1);
 
-
-const weekend = ["Saturday", "Sunday"]
-const weekday = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
-
+const weekend = ["Saturday", "Sunday"];
+const weekday = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
 
 if (weekend.includes(day)) {
-    console.log(`${day} is a weekend.`)
+  console.log(`${day} is a weekend.`);
 } else if (weekday.includes(day)) {
-    console.log(`${day} is a working day.`)
+  console.log(`${day} is a working day.`);
 }
 
 // 4. Write a program which tells the number of days in a month
 // 5. Write a program which tells the number of days in a month, now consider leap year
-const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 
-month = prompt("Enter a month: ").toLowerCase()
-month = month.charAt(0).toUpperCase() + month.slice(1)
-let monthNum = months.indexOf(month) + 1
+month = prompt("Enter a month: ").toLowerCase();
+month = month.charAt(0).toUpperCase() + month.slice(1);
+let monthNum = months.indexOf(month) + 1;
 
-const daysInMonth = new Date(2023, monthNum, 0).getDate()
+const daysInMonth = new Date(2023, monthNum, 0).getDate();
 
-console.log(`${month} has ${daysInMonth} days`)
+console.log(`${month} has ${daysInMonth} days`);
